7a7d165db882802f7e14ad48fcbc3a1f85532878
moamen2020 <m01144944981@gmail.com>
2020-11-14 17:00:25 +0200

    first commit

---
+++ src/components/InfoSection/Data.js
@@ -0,0 +1,52 @@
+export const homeObjOne = {
+    id: 'about',
+    lightBg: false,
+    lightText: true,
+    lightTextDesc: true,
+    topLine: 'Premium Bank',
+    headline: 'Unlinired Transaction with zero fees',
+    description: 'Get access to our exclusive app that allows you to send unlimited transactions without getting charged any sees.',
+    buttonLabel: 'Get started',
+    imgStart: false,
+    image: require('../../images/svg-3.svg'),
+    alt: 'Car', 
+    dark: true,
+    primary: true,
+    darkText: false
+};
+
+
+export const homeObjTwo = {
+    id: 'discover',
+    lightBg: true,
+    lightText: false,
+    lightTextDesc: false,
+    topLine: 'Unlimited Access',
+    headline: 'Login to your account at any time',
+    description: 'Get access to our exclusive app that allows you to send unlimited transactions without getting charged any sees.',
+    buttonLabel: 'Learn More',
+    imgStart: false,
+    image: require('../../images/svg-1.svg'),
+    alt: 'Piggybank', 
+    dark: false,
+    primary: false,
+    darkText: true
+};
+
+
+export const homeObjThree = {
+    id: 'signup',
+    lightBg: true,
+    lightText: false,
+    lightTextDesc: false,
+    topLine: 'Join out Team',
+    headline: 'Creating an account is extremely easy',
+    description: "Get ecertthing set up and in under 10 minutes. all you need to do is add inforamtion and you'rs ready to go. ",
+    buttonLabel: 'Start Now',
+    imgStart: false,
+    image: require('../../images/svg-4.svg'),
+    alt: 'Paper', 
+    dark: false,
+    primary: false,
+    darkText: true
+};
